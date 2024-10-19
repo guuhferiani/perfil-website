3f13047f55711f2ca9721162a6c9e75ca5bbf383
Gustavo Feriani <guuh.feriani@gmail.com>
2024-10-19 10:10:14 -0300

    Update AOS script.js

--- js/script.js
+++ js/script.js
@@ -32,3 +32,15 @@ menuLinks.forEach(item => {
         item.classList.add("active");
     })
 })
+
+/**
+     * Animation on scroll
+     */
+window.addEventListener('load', () => {
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+        mirror: false
+    })
+});
